9929afd64fa0b3276b226ec4de870bcb18afc0cc
Blas Medina <r.blas.m.c@gmail.com>
2021-05-30 01:23:49 -0400

    se agrega el controlador de productos

---
+++ src/controllers/products.controller.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from 'express';
+import ProductsRepository from '../repositories/products.repository';
+
+const autor = {
+  name: 'Blas',
+  lastname: 'Medina',
+};
+
+export class ProductsController {
+  static async findProducts(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { searchInput } = req.params;
+      const items = await ProductsRepository.findProducts(searchInput);
+      return res.json({ autor, items });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async findProduct(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { searchInput } = req.params;
+      const item = await ProductsRepository.findProduct(searchInput);
+      return res.json({ autor, item });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
