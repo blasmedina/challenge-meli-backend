874450688a00c4ed53a8fc42f1628592a5c61b16
Blas Medina <r.blas.m.c@gmail.com>
2021-05-30 01:19:25 -0400

    se agrega repositorio de productos

---
+++ src/repositories/products.repository.ts
@@ -0,0 +1,54 @@
+import { ApiMeliService } from '../services/api-meli.service';
+
+export default class ProductsRepository {
+  static async findProduct(productId: string) {
+    const [productResponse, descriptionResponse] = await Promise.all([
+      ApiMeliService.findProduct(productId),
+      ApiMeliService.findProductDescription(productId),
+    ]);
+    const {
+      id,
+      title,
+      price,
+      currency_id,
+      thumbnail: picture,
+      condition,
+      shipping: { free_shipping },
+      sold_quantity,
+    } = productResponse;
+    const { plain_text: description } = descriptionResponse;
+    return {
+      id,
+      title,
+      price: {
+        currency: currency_id,
+        amount: price,
+        decimals: 0,
+      },
+      picture,
+      condition,
+      free_shipping,
+      sold_quantity,
+      description,
+    };
+  }
+
+  static async findProducts(query: string) {
+    const response = await ApiMeliService.findProducts(query);
+    const results = response.results
+      .slice(0, 4)
+      .map(({ id, title, price, currency_id, thumbnail: picture, condition, shipping: { free_shipping } }) => ({
+        id,
+        title,
+        price: {
+          currency: currency_id,
+          amount: price,
+          decimals: 0,
+        },
+        picture,
+        condition,
+        free_shipping,
+      }));
+    return results;
+  }
+}
